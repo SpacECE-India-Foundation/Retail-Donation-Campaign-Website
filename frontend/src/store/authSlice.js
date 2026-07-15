f4fa9fd0b2aa3fd169719ef92ee64267e28fdd98
Purva Tayade <pptayade07@gmail.com>
2026-07-15 16:14:47 +0530

    Add Redux Toolkit store (auth, donation, ui slices) and React Query provider

---
+++ frontend/src/store/authSlice.js
@@ -0,0 +1,22 @@
+﻿import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isAuthenticated: false,
+    admin: null,
+  },
+  reducers: {
+    setAuthenticated: (state, action) => {
+      state.isAuthenticated = true;
+      state.admin = action.payload;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.admin = null;
+    },
+  },
+});
+
+export const { setAuthenticated, logout } = authSlice.actions;
+export default authSlice.reducer;
---
+++ frontend/src/store/index.js
@@ -0,0 +1,12 @@
+﻿import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import donationReducer from "./donationSlice";
+import uiReducer from "./uiSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    donation: donationReducer,
+    ui: uiReducer,
+  },
+});
