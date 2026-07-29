ba4f578e4f69a0a765f8d322d4f31bbf061f07ea
Aadityaa45 <aaditya.joshi@avantika.edu.in>
2026-07-29 18:06:37 +0530

    Add functionality to fetch all admins for super admin, including campaign details

--- Donation_Drive_Backennd/routes/superAdminOperations.routes.js/accounts.superAdmin.js
+++ Donation_Drive_Backennd/routes/superAdminOperations.routes.js/accounts.superAdmin.js
@@ -4,11 +4,13 @@ import { adminAuth } from "../../middelwares/adminAuth.middelware.js"
 import { authorize } from "../../middelwares/RBAC.middelware.js"
 import { deleteAdminAccount } from "../../controllers/superAdminOperations/manageAdmins.superAdminOperations.controller.js"
 import { transferCampaignManagement } from "../../controllers/superAdminOperations/manageAdmins.superAdminOperations.controller.js"
+import { fetchAllAdmins } from "../../controllers/superAdminOperations/manageAdmins.superAdminOperations.controller.js"
 
 const superAdminOperations = express.Router()
 
 superAdminOperations.post('/add-admin',adminAuth,authorize("SUPER_ADMIN"),addNewAdmin)
 superAdminOperations.post('/delete/:adminId',adminAuth,authorize("SUPER_ADMIN"),deleteAdminAccount)
 superAdminOperations.post('/transfer-management',adminAuth,authorize("SUPER_ADMIN"),transferCampaignManagement)
+superAdminOperations.get('/all-admins',adminAuth,authorize("SUPER_ADMIN"),fetchAllAdmins)
 
 export default superAdminOperations
